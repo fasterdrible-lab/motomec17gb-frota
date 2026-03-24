1136daa8dc91b7912abd28a747adc237dec1da95
fasterdrible-lab <fasterdrible@gmail.com>
2026-03-24 00:58:01 +0000

    Update App.jsx content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ import './styles/App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/motomec17gb-frota">
       <div className="app-layout">
         <Sidebar />
         <div className="main-content">
